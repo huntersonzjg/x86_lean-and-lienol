971806bb049b5f93404bbb92c50b2ce474e2d410
huntersonzjg <78995644+huntersonzjg@users.noreply.github.com>
2022-10-20 15:57:28 +0800

    fenzo-add-feeds

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,8 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#modfied as follow
-#git clone https://github.com/kenzok8/openwrt-packages.git  feeds/kenzo
-#git clone https://github.com/kenzok8/small.git  feeds/small
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
